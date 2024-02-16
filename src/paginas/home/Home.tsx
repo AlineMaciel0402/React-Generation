3e52297698b24a3d5bccba5fab1f61c7c85939db
AlineMaciel0402 <alinemaciel04@gmail.com>
2024-02-16 15:49:16 -0300

    Home

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,14 @@
+import "./Home.css"
+
+function Home() {
+return (
+<>
+<h1 className="titulo">Manipulando o mundo</h1>
+<img className="img" src = "https://c.wallhere.com/photos/51/8a/anime_anime_girls-1510673.jpg!d" />
+
+</>
+)
+
+}
+
+export default Home;
